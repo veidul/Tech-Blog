17fd99db7a072d11027ae46c9d98d307ed5943a8
ludie lambright <ludiefletcher@aol.com>
2022-01-02 22:56:14 -0800

    wrote out models/index.js&&user.js

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
+
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id'
+})
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id'
+});
+
+module.exports = { User, Blog, Comment };
